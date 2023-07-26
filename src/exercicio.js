fce608897e67b18ff962670e293d287c6458e140
Renan <renanpellegrini@outlook.com>
2023-07-26 10:27:14 -0300

    commit inicial

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function calculaArea(base, altura) {
+    return base * altura;
+}
+calculaArea(7, 8);
+function saudacao(nome) {
+    console.log(`Olá, ${nome}`);
+}
+saudacao('Renan');
---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+function calculaArea(base: number, altura: number): number{
+    return base * altura
+}
+
+calculaArea(7, 8)
+
+function saudacao(nome: String) {
+    console.log(`Olá, ${nome}`)
+}
+
+saudacao('Renan')
